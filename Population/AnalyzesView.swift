7703e6c176b293ac63695773af687be5e52b1309
Can Balkaya <canbalkya@gmail.com>
2019-12-18 18:18:17 +0300

    Added list

--- Population/AnalyzesView.swift
+++ Population/AnalyzesView.swift
@@ -10,28 +10,27 @@ import SwiftUI
 
 struct AnalyzesView: View {
     var body: some View {
-        HStack {
-            VStack (alignment: .trailing, spacing: UIScreen.main.bounds.height * 0.12) {
-                Spacer().frame(height: 1)
-                Text(String(humansLast.last!)).font(.system(size: 8))
-                Text(String((humansLast.last! + humansLast.first!)/2)).font(.system(size: 8))
-                Text(String(humansLast.first!)).font(.system(size: 8)).padding(.bottom, 20)
-                Spacer().frame(height: 4)
-            }.frame(height: UIScreen.main.bounds.height * 0.3)
+        VStack {
+            HStack {
+                VStack (alignment: .trailing, spacing: UIScreen.main.bounds.height * 0.12) {
+                    Spacer().frame(height: 1)
+                    Text(String(humansLast.max()!)).font(.system(size: 8))
+                    Text(String((humansLast.max()! + humansLast.min()!)/2)).font(.system(size: 8))
+                    Text(String(humansLast.min()!)).font(.system(size: 8)).padding(.bottom, 20)
+                    Spacer().frame(height: 4)
+                }.frame(height: UIScreen.main.bounds.height * 0.3)
+                
+                HStack (spacing: 12) {
+                    ForEach (0...humansLast.count - 1, id: \.self) { humanLast in
+                        BarView(value: humansLast[humanLast], currentGeneration: humanLast + 1)
+                    }
+                }
+            }.navigationBarTitle("Analyzes")
             
-            HStack (spacing: 12) {
-                BarView(value: humansLast[0], currentGeneration: 1)
-                BarView(value: humansLast[1], currentGeneration: 2)
-                BarView(value: humansLast[2], currentGeneration: 3)
-                BarView(value: humansLast[3], currentGeneration: 4)
-                BarView(value: humansLast[4], currentGeneration: 5)
-                BarView(value: humansLast[5], currentGeneration: 6)
-                BarView(value: humansLast[6], currentGeneration: 7)
-                BarView(value: humansLast[7], currentGeneration: 8)
-                BarView(value: humansLast[8], currentGeneration: 9)
-                BarView(value: humansLast[9], currentGeneration: 10)
+            List (0...humansLast.count - 1, id: \.self) { humanLast in
+                Text("\(humansLast[humanLast]) people in \(humanLast + 1). generation")
             }
-        }.navigationBarTitle("Analyzes")
+        }
     }
 }
 
