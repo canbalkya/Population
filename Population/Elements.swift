77dfe1bc57931c9ed8b68f4a3c15c15a39cde06a
Can Balkaya <canbalkya@gmail.com>
2020-01-21 16:30:12 +0000

    Developed some things

---
+++ Population/Elements.swift
@@ -0,0 +1,16 @@
+//
+//  Elements.swift
+//  Population
+//
+//  Created by Can Balkaya on 1/21/20.
+//  Copyright © 2020 Can Balkaya. All rights reserved.
+//
+
+import Foundation
+
+var humans = [Int]()
+var humansLast = [Int]()
+var human = Int()
+var newGeneration = Int()
+var oldGeneration = Int()
+var adultPeople = Int()
